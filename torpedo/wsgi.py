a424c183f32e8cb7b17c8a90b97eb11dc4a8d56f
teerytko <teemu.rytkonen@gmail.com>
2015-07-24 11:17:50 +0000

    Refactor the main folder name
    -Change the mydemo to torpedo

--- torpedo/wsgi.py
+++ torpedo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mydemo project.
+WSGI config for torpedo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mydemo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "torpedo.settings")
 
 application = get_wsgi_application()
