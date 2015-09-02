27570b33a4256c057cd6ba10aa0aa81e9ba73940
tsrytkon <teemu.s.rytkonen@intel.com>
2015-09-02 20:49:35 +0300

    Add delete support

--- statistics/static/js/main.view.js
+++ statistics/static/js/main.view.js
@@ -2,37 +2,51 @@
 
 require(['jquery', 'bootstrap', 'jquerycookie'], 
   function($, bootstrap, cookie) {
-  	function submit(e) {
-			event.preventDefault();
-			var name = $('#name').val();
-			var csrftoken = $.cookie('csrftoken');
-		  console.log( "Saving "+$('#name').val() );
-			console.log(POSTURL);
+    var csrftoken = $.cookie('csrftoken');
+    function onSubmit(e) {
+      event.preventDefault();
+      var name = $('#name').val();
+      $.ajax({
+        type: "POST",
+        headers: {"X-CSRFToken": csrftoken},
+        url: POSTURL,
+        data: {'name': name,
+                "from_date": null,
+                "to_date": null
+                },
+        success: function(data)
+        {
+          location.reload();
+        }
+       });
+      return false;
+    };
+    function onDelete(e) {
+      event.preventDefault();
+      var rowid = $( this ).parents('.datarow').data('id');
+      var resurl = POSTURL+rowid+"/"
 
-		  $.ajax({
-	      type: "POST",
-	      headers: {"X-CSRFToken": csrftoken},
-	      url: POSTURL,
-	      data: {'name': name, 
-	      			  "from_date": null,
-								"to_date": null
-								},
-	      success: function(data)
-	      {
-	      	console.log("Saved ok");
+      $.ajax({
+        type: "DELETE",
+        headers: {"X-CSRFToken": csrftoken},
+        url: resurl,
+        success: function(data)
+        {
+          location.reload();
+        }
+       });
+      return false;
+    };
 
-	      }
-	     });
-			return false;
-		};
-
-  	$('#newleague').on('submit', submit);
-  	$('.datarow').mouseover(function() {
-  		$( this ).find( "span" ).show();
-  	}).mouseout(function() {
-  		$( this ).find( "span" ).hide();
-  	});
-	});
+    $('.datarow span').css('visibility', 'hidden');
+    $('#newleague').on('submit', onSubmit);
+    $('.datarow').mouseover(function() {
+      $( this ).find( "span" ).css('visibility', 'visible');
+    }).mouseout(function() {
+      $( this ).find( "span" ).css('visibility', 'hidden');
+    });
+    $('.datarow span').click(onDelete);
+  });
 
 
 }).call(this);
